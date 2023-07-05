4ab030817c7c681b36107c6cf3bcc70d1fb48156
ismail-icanovic <ismail.icanovic@allops.co>
2023-07-05 11:55:43 +0200

    preparation done

--- commands.sh
+++ commands.sh
@@ -1,6 +1,7 @@
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-source ~/.zshrc
-source ~/.zprofile
+#!/bin/bash
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+#source ~/.zshrc
+#source ~/.zprofile
 nvm --version
 nvm install 18.0.0
 nvm use 18.0.0
