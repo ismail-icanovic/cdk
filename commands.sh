8dc7a321c72d3e08a65080eb67ed57fa4d18b1a4
ismail-icanovic <ismail.icanovic@allops.co>
2023-07-05 11:49:07 +0200

    CDK init with a file sommands.sh that does the job

---
+++ commands.sh
@@ -0,0 +1,8 @@
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+source ~/.zshrc
+source ~/.zprofile
+nvm --version
+nvm install 18.0.0
+nvm use 18.0.0
+node --version
+cdk init app --language typescript
